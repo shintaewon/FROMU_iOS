6d3d092e304d63c2ad9434f092169699e8f93925
shintaewon <tlsxodnjs147@gmail.com>
2023-02-21 23:34:00 +0900

    init: 프로젝트 생성

---
+++ FromU/FromU/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FromU
+//
+//  Created by 신태원 on 2023/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

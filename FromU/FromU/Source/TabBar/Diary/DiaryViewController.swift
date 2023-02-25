b9be07232892a31a6e8d0e0cfd8fbc976ee10b1e
shintaewon <tlsxodnjs147@gmail.com>
2023-02-25 11:26:03 +0900

    2.25 모각코 시작

---
+++ FromU/FromU/Source/TabBar/Diary/DiaryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DiaryViewController.swift
+//  FromU
+//
+//  Created by 신태원 on 2023/02/22.
+//
+
+import UIKit
+
+class DiaryViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("얘가 나오는겨?")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FromU/Pods/Target Support Files/Pods-FromUTests/Pods-FromUTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FromUTests : NSObject
+@end
+@implementation PodsDummy_Pods_FromUTests
+@end
